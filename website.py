11c37df4a3aacc1a49eff67c1842691347848897
nikoreano <73408102+nikoreano@users.noreply.github.com>
2025-10-16 14:31:22 -0300

    Add files via upload

---
+++ website.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.header("Bienvenido!", divider="gray")
+st.subheader("Aqui estan los enlaces a las carpetas que necesites revisar")
+
+st.subheader("Nodo H100", divider="gray")
+st.markdown("[Rack H100](https://drive.google.com/drive/folders/1tq2eka2indj5P-wVLr8RP_b_ANasEcNt?usp=sharing)")
+
+st.markdown("[Bodega H100](https://drive.google.com/drive/folders/1Xs0smj6FioL5-04ZuDTfdiWCwX9oQfhJ?usp=sharing)")
+
+st.subheader("Nodo SPX", divider="gray")
+st.markdown("[Rack SPX](https://drive.google.com/drive/folders/1CkMzxPEpyOCBsMCzH5wnyqZgVy5H1MfU?usp=sharing)")
